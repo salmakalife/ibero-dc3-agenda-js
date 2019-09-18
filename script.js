970dadd2e26a3c882f39eb4f99538b595dcf84e4
Salma Kalife <salmakalife@hotmail.es>
2019-09-17 20:53:40 -0500

    Ediciones Finales

--- script.js
+++ script.js
@@ -9,3 +9,165 @@ var contactos = [
     telefono: "5555555555" }
 ];
 
+function abrirVentana(){
+  document.getElementById("modal").classList.remove("modal");
+  document.getElementById("modal").classList.add("modal-open");
+}
+
+function cerrarVentana(){
+  document.getElementById("modal").classList.add("modal");
+  document.getElementById("modal").classList.remove("modal-open");
+}
+
+function validarCampos(){
+  var nombre = document.getElementById("nombre").value;
+  var mail = document.getElementById("email").value;
+  var empresa = document.getElementById("empresa").value;
+  var telefono = document.getElementById("telefono").value;
+  var errores = document.getElementById("errores");
+  errores.innerHTML="";
+  
+  var longnombre = validarLongitud(nombre);
+  var longmail = validarLongitud(mail);
+  var pricar = validarPrimerCaracter(mail);
+  var arroba = validarArroba(mail);
+  var dominio = validarDominio(mail);
+  flag = true;
+
+  if(!longnombre){
+      var error = document.createElement("li");
+      error.innerHTML="El nombre no puede estar vacio";
+      errores.appendChild(error);
+      flag = false;
+  }
+
+  if (longmail && pricar && arroba && dominio){
+      // No pasa nada
+
+  }else{
+      var error1 = document.createElement("li");
+      error1.innerHTML = "El email no es valido";
+      errores.appendChild(error1);
+      flag = false;
+
+  }
+
+  if(flag){
+    contactos.push({nombre: nombre, empresa: empresa, email: mail, telefono: telefono});
+    imprimirTodosLosContactos(); 
+    document.getElementById("modal").classList.add("modal");
+    document.getElementById("modal").classList.remove("modal-open");
+    var nombre = document.getElementById("nombre").value="";
+    var mail = document.getElementById("email").value="";
+    var empresa = document.getElementById("empresa").value="";
+    var telefono = document.getElementById("telefono").value="";
+  }
+}
+  function validarLongitud(nombres){
+  var n = nombres.length;
+  if (n >= 2){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+function validarLongitudEmail(correo){
+  var n = correo.length;
+  if (n >= 6){
+      return true;
+  } else {
+      return false;
+  }
+}
+
+function validarPrimerCaracter(correo){
+if ((correo.charCodeAt(0) >= 65 && correo.charCodeAt(0) <= 90) || (correo.charCodeAt(0) >= 97 && correo.charCodeAt(0) <= 122)){
+  return true;
+}else{
+  return false;
+}
+}
+
+function validarArroba(correo){
+var cuantasArrobas = 0;
+var posArroba = 0;
+for (var i=0; i<correo.length; i++){
+  if (correo.charAt(i) == "@"){
+    cuantasArrobas++;
+    posArroba = i;
+  }
+}
+
+if (cuantasArrobas == 1){
+  if (posArroba >=1 && posArroba <= correo.length - 5){
+    return true;
+  }else{
+    return false;
+  }
+}else{
+  return false;
+}	
+}
+
+function validarDominio(correo){
+var pos = correo.lastIndexOf(".");
+if ((correo.length-1 - pos >=2) && (correo.length-1 - pos <=4)){
+  for (var i=pos+1; i<correo.length; i++){
+    if (correo.charCodeAt(i)<65 || (correo.charCodeAt(i)>90 && correo.charCodeAt(i) < 97) || correo.charCodeAt(i) > 122){
+      return false;
+      break;
+    }
+  }
+  return true;
+}else{
+  return false;
+}
+
+}
+
+function imprimirContactos(contacto, index){
+  var lista = document.getElementById("contactos");
+    lista.insertAdjacentHTML('beforeend',
+        `<li class="contacto">
+        <div class="actions">
+        <a><i class="fa fa-trash" onclick="eliminarContacto(`+ index +`)"></i></a>
+        </div>
+        <i class="userIcon fa fa-user"></i>
+        <h4 class="nombre">`+ contacto.nombre + `</h4>
+        <div class="datos">
+          <div class="dato">
+            <i class="fa fa-building"></i>
+            <span>`+ contacto.empresa +`</span>
+          </div>
+          <div class="dato">
+            <i class="fa fa-envelope"></i>
+            <a href="mailto:`+ contacto.email +`">`+ contacto.email +`</a>
+          </div>
+          <div class="dato">
+            <i class="fa fa-phone"></i>
+            <a href="tel:`+ contacto.telefono +`">`+ contacto.telefono +`</a>
+          </div>
+        </div>
+      </li>
+    </li>`);
+}
+
+
+function eliminarContacto(index){
+  contactos.splice(index, 1);
+  imprimirTodosLosContactos();
+}
+function marcarCompletado(index){
+  contactos[index].completado = !contactos[index].completado;
+  imprimirTodosLosContactos();
+}
+
+function imprimirTodosLosContactos() {
+  document.getElementById("contactos").innerHTML = "";
+  contactos.forEach(imprimirContactos);
+}
+
+
+imprimirTodosLosContactos();
